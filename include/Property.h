362d390f142bea5c881f6d10939613946f3df61e
tezz-io <matblockgreninja@gmail.com>
2021-07-16 06:06:40 -0700

    [Process] Added a generic type called property to handle properties.

---
+++ include/Property.h
@@ -0,0 +1,30 @@
+#include TEZZGL_PROPERTY_H
+#define TEZZGL_PROPERTY_H
+
+template <typename T>
+class Property {
+private:
+    T value;
+public:
+    Property( T _value ) {
+        value = _value;
+    }
+
+    T& operator=( const T& _value ) {
+        value = _value;
+    }
+
+    const T& operator()() const {
+        return value;
+    }
+
+    explicit operator const T& () const {
+        return value;
+    }
+
+    T* operator->() {
+        return &value;
+    }
+}
+
+#endif
