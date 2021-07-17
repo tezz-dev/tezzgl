a0ba69f153765f74feb8307b1e5da4f313f0a99e
tezz-io <matblockgreninja@gmail.com>
2021-07-17 07:36:00 -0700

    [Process] Added BitDepth class to record the red green and blue bit depths.

---
+++ include/BitDepth.h
@@ -0,0 +1,17 @@
+#ifndef TEZZGL_BITDEPTH_H
+#define TEZZGL_BITDEPTH_H
+
+class BitDepth {
+public:
+    int red;
+    int green;
+    int blue;
+
+    BitDepth( int _red, int _green, int _blue ) {
+        red = _red;
+        green = _green;
+        blue = _blue;
+    }
+}
+
+#endif
