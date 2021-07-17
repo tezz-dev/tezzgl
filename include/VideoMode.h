085ebf5f62ab44894ec7909e31b1b1ef20eb7b5e
tezz-io <matblockgreninja@gmail.com>
2021-07-17 07:52:24 -0700

    [Process] Added GLFWvidmode constructor and set method for VideoMode class.

--- include/VideoMode.h
+++ include/VideoMode.h
@@ -9,10 +9,24 @@ public:
     BitDepth bitDepth;
     RefreshRate refreshRate;
 
+    VideoMode( GLFWvidmode mode ):
+        size(mode.x, mode.y),
+        bitDepth(mode.redBits, mode.greenBits, mode.blueBits),
+        refreshRate(mode.refreshRate) {}
+        
     VideoMode( Size _size, BitDepth _bitDepth, RefreshRate _refreshRate ):
         size(_size),
         bitDepth(_bitDepth),
         refreshRate(_refreshRate) {}
+
+    void set( GLFWvidmode mode ) {
+        videoMode.size.x = mode.x;
+        videoMode.size.y = mode.y;
+        videoMode.bitDepth.red = mode.redBits;
+        videoMode.bitDepth.green = mode.greenBits;
+        videoMode.bitDepth.blue = mode.blueBits;
+        videoMode.refreshRate = mode.refreshRate;
+    }
 }
 
 #endif
