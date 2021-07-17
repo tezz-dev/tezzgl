8c46268b25e712f849f8189d8ab1d38a6654e09e
tezz-io <matblockgreninja@gmail.com>
2021-07-17 03:45:03 -0700

    [Process] Added Properties for Point2d, Point2f, POffset, PSize and PPosition.

--- include/Definitions.h
+++ include/Definitions.h
@@ -33,6 +33,26 @@
     #define Position Point2<int>
 #endif
 
+#ifndef PPoint2f
+    #define PPoint2f Property< Point2<float> >
+#endif
+
+#ifndef PPoint2d
+    #define PPoint2d Property< Point2<int> >
+#endif
+
+#ifndef POffset
+    #define POffset Property< Point2<float> >
+#endif
+
+#ifndef PSize
+    #define PSize Property< Point2<int> >
+#endif
+
+#ifndef PPosition
+    #define PPosition Property< Point2<int> >
+#endif
+
 #ifndef width
     #define width x
 #endif
