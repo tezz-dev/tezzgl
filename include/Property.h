1091b7a97261e7c9c1ef7654b3ce832ed7272a36
tezz-io <matblockgreninja@gmail.com>
2021-07-17 03:59:58 -0700

    [Process] Added window to the property so that glfw functions can be easily implemented.

--- include/Property.h
+++ include/Property.h
@@ -1,19 +1,28 @@
-#include TEZZGL_PROPERTY_H
+#ifndef TEZZGL_PROPERTY_H
 #define TEZZGL_PROPERTY_H
+
+#include <functional>
+#include <GLFW/glfw3.h>
 
 template <typename T>
 class Property {
 private:
     T value;
+    GLFWwindow* window;
 public:
-    auto set = []( T value ) {};
+    std::function< void(T, GLFWwindow*) > set = []( T value, GLFWwindow* window ) {};
 
-    Property( T _value ) {
-        value = _value;
+    Property( T _value ):
+        value(_value) {
+        window = nullptr;
+    }
+
+    void setWindow( GLFWwindow* _window ) {
+        window = _window;
     }
 
     T& operator=( const T& _value ) {
-        set( value );
+        set( value, window );
         value = _value;
     }
 
@@ -28,6 +37,6 @@ public:
     T* operator->() {
         return &value;
     }
-}
+};
 
 #endif
