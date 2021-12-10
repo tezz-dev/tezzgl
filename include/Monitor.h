31c15aadd33239741a04b8dc2247f7e2e2a18693
Tejas M R <matblockgreninja@gmail.com>
2021-12-10 17:24:15 +0000

    Added Monitor class

--- include/Monitor.h
+++ include/Monitor.h
@@ -1,10 +1,23 @@
 #ifndef TEZZGL_MONITOR_H
 #define TEZZGL_MONITOR_H
 
-#include "GLFW/glfw."
+#include "GLFW/glfw3.h"
+#include <vector>
 
 class Monitor {
-    GLFWmonitor* monitor
+    GLFWmonitor* monitor;
+
+    Monitor( GLFWmonitor* monitor ): monitor(monitor) {}
+
+    static std::vector<Monitor> getMonitors() {
+        int numberOfMonitors;
+        GLFWmonitor** monitorRefs = glfwGetMonitors(&numberOfMonitors);
+        std::vector<Monitor> monitors;
+        for( int i=0; i<numberOfMonitors; i++ ) {
+            monitors.push_back({monitorRefs[i]});
+        }
+        return monitors;
+    }
 };
 
 #endif
