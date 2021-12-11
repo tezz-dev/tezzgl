1ee00c01c26705bd41a2e2b9c66602eb782e1da5
Tejas M R <matblockgreninja@gmail.com>
2021-12-11 09:27:24 +0000

    Added position and physicalSize

--- include/Monitor.h
+++ include/Monitor.h
@@ -3,21 +3,47 @@
 
 #include "GLFW/glfw3.h"
 #include <vector>
+#include "Definitions.h"
 
 class Monitor {
+private:
     GLFWmonitor* monitor;
-
+public:
     Monitor( GLFWmonitor* monitor ): monitor(monitor) {}
-
-    static std::vector<Monitor> getMonitors() {
-        int numberOfMonitors;
-        GLFWmonitor** monitorRefs = glfwGetMonitors(&numberOfMonitors);
-        std::vector<Monitor> monitors;
-        for( int i=0; i<numberOfMonitors; i++ ) {
-            monitors.push_back({monitorRefs[i]});
-        }
-        return monitors;
-    }
+    static std::vector<Monitor> monitors();    
+    static Monitor primaryMonitor();
+    Position position();
+    Size physicalSize();
 };
+
+inline
+std::vector<Monitor> Monitor::monitors() {
+    int numberOfMonitors;
+    GLFWmonitor** monitorRefs = glfwGetMonitors(&numberOfMonitors);
+    std::vector<Monitor> monitors;
+    for( int i=0; i<numberOfMonitors; i++ ) {
+        monitors.push_back({monitorRefs[i]});
+    }
+    return monitors;
+}
+
+inline
+Monitor Monitor::primaryMonitor() {
+    return {glfwGetPrimaryMonitor()};
+}
+
+inline
+Position Monitor::position() {
+    int width, height;
+    glfwGetMonitorPos(monitor, &width, &height);
+    return {width, height};
+}
+
+inline
+Size Monitor::physicalSize() {
+    int widthMM, heightMM;
+    glfwGetMonitorPhysicalSize(monitor, &widthMM, &heightMM);
+    return {widthMM, heightMM};
+}
 
 #endif
