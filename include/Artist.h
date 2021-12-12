10b3385d7fa467e7d475f1b60e7f9f007f33b8a5
tezz-io <matblockgreninja@gmail.com>
2021-12-12 17:48:10 +0530

    Removed definitions from Artist.h

--- include/Artist.h
+++ include/Artist.h
@@ -9,55 +9,14 @@
 
 class Artist {
 public:
-    static void UseColor( float, float, float );
-    static void DrawGrid( Offset, Grid, Size );
-    static void DrawRect(
+    static void useColor( float, float, float );
+    static void drawGrid( Offset, Grid, Size );
+    static void drawRect(
         Point2f,
         Point2f,
         Offset = {0.0, 0.0},
         Size = {1, 1}
     );
 };
-
-void Artist::UseColor( float red, float green, float blue ) {
-    glColor3f( red, green, blue );
-}
-
-void Artist::DrawGrid( Offset offset, Grid grid, Size size ) {
-    glBegin( GL_LINES );
-
-    for( int i=0; i<=grid.count.x; i++ ) {
-        glVertex2f (
-            offset.x,
-            offset.y + (float)i * (float)grid.size.width
-        );
-        glVertex2f (
-            (float)size.width - offset.x,
-            offset.y + (float)i * (float)grid.size.width
-        );
-    }
-
-    for( int i=0; i<=grid.count.y; i++ ) {
-        glVertex2f (
-            offset.x + (float)i * (float)grid.size.height,
-            offset.y
-        );
-        glVertex2f (
-            offset.x + (float)i * (float)grid.size.height,
-            (float)size.height - offset.y
-        );
-    }
-
-    glEnd();
-}
-
-void Artist::DrawRect( Point2f p1, Point2f p2, Offset offset, Size size ) {
-    glRectd(
-        offset.x + p1.x * size.width,
-        offset.y + p1.x * size.height,
-        offset.x + p2.x * size.width,
-        offset.y + p2.y * size.height
-    );
-}
 
 #endif
