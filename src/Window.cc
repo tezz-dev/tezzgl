6dda899cd1751408cd75e82fcaa2711b404037c3
Tejas M R <matblockgreninja@gmail.com>
2021-12-17 13:28:09 +0000

    Commented out previous version of Window

--- src/Window.cc
+++ src/Window.cc
@@ -4,19 +4,19 @@
 
 #include "Window.h"
 
-Window::Window( String _title, int _width, int _height ):
-        window(nullptr),
-        title(_title),
-        size({_width, _height}),
-        shareWindow(nullptr),
-        monitor(nullptr) {
-    size.set = []( Size size, GLFWwindow* window ) {
-        glfwSetWindowSize(window, size.x, size.y);
-    };
-}
+// Window::Window( String _title, int _width, int _height ):
+//         window(nullptr),
+//         title(_title),
+//         size({_width, _height}),
+//         shareWindow(nullptr),
+//         monitor(nullptr) {
+//     size.set = []( Size size, GLFWwindow* window ) {
+//         glfwSetWindowSize(window, size.x, size.y);
+//     };
+// }
 
-Window::Window( String _title, Size _size ):
-        Window(_title, _size.x, _size.y) {}
+// Window::Window( String _title, Size _size ):
+//         Window(_title, _size.x, _size.y) {}
 
 // GLFWwindow* Window::GetWindow() { return window; }
 // Size Window::GetSize() {
@@ -33,78 +33,78 @@ Window::Window( String _title, Size _size ):
 // }
 // void Window::SetMonitor( Monitor _monitor ) { monitor = _monitor; }
 // void Window::SetShareWindow( ShareWindow _shareWindow ) { shareWindow = _shareWindow; }
-void Window::Init() {
-    if( !glfwInit() )
-        return;
+// void Window::Init() {
+//     if( !glfwInit() )
+//         return;
 
-    window = glfwCreateWindow(
-            (int)size().width,
-            (int)size().height,
-            title,
-            monitor,
-            shareWindow
-    );
+//     window = glfwCreateWindow(
+//             (int)size().width,
+//             (int)size().height,
+//             title,
+//             monitor,
+//             shareWindow
+//     );
 
-    size.setWindow( window );
+//     size.setWindow( window );
 
-    if( !window ) {
-        glfwTerminate();
-        return;
-    }
+//     if( !window ) {
+//         glfwTerminate();
+//         return;
+//     }
 
-    glfwMakeContextCurrent( window );
-}
-void Window::StartRendering() {
-    glClear(GL_COLOR_BUFFER_BIT);
-}
-void Window::Resize() {
-    int _width, _height;
-    glfwGetFramebufferSize (
-            window,
-            &_width,
-            &_height
-    );
-    size->width = _width;
-    size->height = _height;
+//     glfwMakeContextCurrent( window );
+// }
+// void Window::StartRendering() {
+//     glClear(GL_COLOR_BUFFER_BIT);
+// }
+// void Window::Resize() {
+//     int _width, _height;
+//     glfwGetFramebufferSize (
+//             window,
+//             &_width,
+//             &_height
+//     );
+//     size->width = _width;
+//     size->height = _height;
 
-    glViewport(0, 0, size->width, size->height);
-}
-void Window::Setup() const {
-    glMatrixMode( GL_PROJECTION );
+//     glViewport(0, 0, size->width, size->height);
+// }
+// void Window::Setup() const {
+//     glMatrixMode( GL_PROJECTION );
 
-    glLoadIdentity();
+//     glLoadIdentity();
 
-    glClear( GL_COLOR_BUFFER_BIT );
+//     glClear( GL_COLOR_BUFFER_BIT );
 
-    glOrtho (
-            0,
-            size().width,
-            size().height,
-            0,
-            0,
-            1
-    );
-}
-void Window::EndRendering() {
-    glfwSwapBuffers( window );
+//     glOrtho (
+//             0,
+//             size().width,
+//             size().height,
+//             0,
+//             0,
+//             1
+//     );
+// }
+// void Window::EndRendering() {
+//     glfwSwapBuffers( window );
 
-    glfwPollEvents();
-}
-void Window::Destroy() {
-    glfwTerminate();
-}
-bool Window::ShouldClose() {
-    return glfwWindowShouldClose( window );
-}
-void Window::SetWindowSizeLimits( Size minSize, Size maxSize) {
-    glfwSetWindowSizeLimits(
-            window,
-            minSize.x,
-            minSize.y,
-            maxSize.x,
-            maxSize.y
-    );
-}
+//     glfwPollEvents();
+// }
+// void Window::Destroy() {
+//     glfwTerminate();
+// }
+// bool Window::ShouldClose() {
+//     return glfwWindowShouldClose( window );
+// }
+// void Window::SetWindowSizeLimits( Size minSize, Size maxSize) {
+//     glfwSetWindowSizeLimits(
+//             window,
+//             minSize.x,
+//             minSize.y,
+//             maxSize.x,
+//             maxSize.y
+//     );
+// }
 
 /*
 GLFW_RESIZABLE
